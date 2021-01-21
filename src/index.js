50c85c921afc8af7cb78b28d8ccb8b5161bbbefc
crissdot <cris-2-11-2001@hotmail.com>
2021-01-20 18:38:38 -0600

    add addTimes function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function addTimes(times) {
+    times.forEach(time => {
+        const timeSplit = time.split(':');
+        if(timeSplit.length === 2) {
+            const min = timeSplit[0];
+            const sec = timeSplit[1];
+        }
+    });
+}
 
+const test = new Array('25:15', '18:57');
+addTimes(test);
