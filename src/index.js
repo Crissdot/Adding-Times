00a9d916099c25b529321473f1e15e90efb701d9
crissdot <cris-2-11-2001@hotmail.com>
2021-01-20 19:03:15 -0600

    add make format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 function addTimes(times) {
+    let sumSeconds = 0;
     times.forEach(time => {
         const timeSplit = time.split(':');
         if(timeSplit.length === 2) {
             const min = timeSplit[0];
             const sec = timeSplit[1];
-            console.log(convertTimeToSeconds(sec, min));
+            sumSeconds += convertTimeToSeconds(sec, min);
         }
     });
+    const [hours, minutes, seconds] = makeFormat(sumSeconds);
+    const msg = `${hours}:${minutes}:${seconds}`;
+    console.log(msg);
+}
+
+function makeFormat(seconds) {
+    let hours = 0, minutes = 0;
+
+    for(let i = 0; seconds > 59; i++) {
+        minutes++;
+        seconds -= 60;
+    }
+    for(let i = 0; minutes > 59; i++) {
+        hours++;
+        minutes -= 60;
+    }
+    return [hours, minutes, seconds];
 }
 
 function convertTimeToSeconds(_sec, _min, _h = 0) {
@@ -14,12 +32,9 @@ function convertTimeToSeconds(_sec, _min, _h = 0) {
     let min = convertStringToNumber(_min);
     let sec = convertStringToNumber(_sec);
 
-    for(let i = 0; i < h; i++) {
-        min += 60;
-    }
-    for(let i = 0; i < min; i++) {
-        sec += 60;
-    }
+    for(let i = 0; i < h; i++) min += 60;
+    for(let i = 0; i < min; i++) sec += 60;
+
     return sec;
 }
 
