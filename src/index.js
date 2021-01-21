39994c05e9a48792b73978edc19b0fe0a8d8f338
crissdot <cris-2-11-2001@hotmail.com>
2021-01-20 18:45:07 -0600

    add convert time to seconds

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ function addTimes(times) {
         if(timeSplit.length === 2) {
             const min = timeSplit[0];
             const sec = timeSplit[1];
+            console.log(convertTimeToSeconds(sec, min));
         }
     });
+}
+
+function convertTimeToSeconds(_sec, _min, _h = 0) {
+    let h = convertStringToNumber(_h);
+    let min = convertStringToNumber(_min);
+    let sec = convertStringToNumber(_sec);
+
+    for(let i = 0; i < h; i++) {
+        min += 60;
+    }
+    for(let i = 0; i < min; i++) {
+        sec += 60;
+    }
+    return sec;
+}
+
+function convertStringToNumber(str) {
+    return parseInt(str);
 }
 
 const test = new Array('25:15', '18:57');
